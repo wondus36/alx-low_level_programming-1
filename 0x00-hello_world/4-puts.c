aa22294b0668a95da1a0b1e7afc80c90425e13e0
firdaushassansalim <firdaushassano33@gmail.com>
2021-06-17 04:46:30 -0700

    changed a few things

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  * Description: prints "\"Programming is like building a multilingual puzzle"
-  * return: 0
-  */
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * return: 0
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
