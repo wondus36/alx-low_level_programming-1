99e54631b85058f632cfa2588245e56014a4a8ca
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-18 11:57:15 +0300

    created a file that prints out whether a number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <time.h>
-
+#include <time.h>
 /**
  * main - entry point
  * Description: prints  if a number is positive or negative.
@@ -8,8 +8,14 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > a)
-printf("%d is positive \n", n)
+printf("%d is positive \n", n);
+else if (n == 0)
+printf("%d is zero \n", n);
+else if (n < a)
+printf("%d is negative \n", n);
+return (0);
 }
