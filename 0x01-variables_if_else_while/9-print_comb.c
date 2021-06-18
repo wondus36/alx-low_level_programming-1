db3a92094db297aae81e9d144e3d89628b7dcc47
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-18 18:17:54 +0300

    prints 0-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9 ; i++)
+	for (i = '0'; i <= '9' ; i++)
 	{
 		putchar(i);
+
 		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
