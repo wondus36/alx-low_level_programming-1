43a1f0175afa51f259ba64e78d9523ed481caa92
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-18 17:12:33 +0300

    prints int base 16 and lower

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Print base 16 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
