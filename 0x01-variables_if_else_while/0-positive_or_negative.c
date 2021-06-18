82d8d98c1fb70de8c48eabc621dc3de9f284fbd8
firdaushassansalim <firdaushassano33@gmail.com>
2021-06-17 23:51:58 -0700

    created a README file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+
+int main(void)
+{
+int n;
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+**Variables, if, else, while**
+#
+In this project, i am doing real-world projects on Variables, is, else, and while.
