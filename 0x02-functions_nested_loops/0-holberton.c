c0074f32f5f5cc227d54a5594537fc0a6b9bb2fe
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-22 13:10:28 +0300

    _putchar tasks

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Results: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "Holberton\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+	_putchar(c);
+	i++;
+	}
+return (0);
+}
