6f91a91d23940e6a245988e21a2c896925198a29
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-22 15:59:00 +0300

    Checks if alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: the integer value it receives
+ * Description: checks if alphabet
+ * Return: 1 if true. 0 if false
+ */
+
+int _isalpha(int c)
+{
+int a = 'a';
+int A = 'A';
+
+for (a = 'a'; a <= 'z'; a++)
+{
+for (A = 'A'; A <= 'A'; A++)
+{
+if (a == c || A == c)
+{
+return (1);
+}
+}
+}
+return (0);
+}
