f61a465f2679573b8b8ee698d57a1b3f262f4465
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-22 13:15:46 +0300

    _putchar tasks

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  * Description: prints 'Holberton \n'
- * Results: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -12,6 +12,7 @@ int i = 0;
 while (str[i] != '\0')
 	{
 	char c = str[i];
+
 	_putchar(c);
 	i++;
 	}
