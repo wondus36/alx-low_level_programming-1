767f317b7bbfb64a5e646ce004f879947c2583de
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-24 13:26:40 +0300

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul(int a, int b) - gives the product of a and b
+ * @a: varible
+ * @b: variable
+ *
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
