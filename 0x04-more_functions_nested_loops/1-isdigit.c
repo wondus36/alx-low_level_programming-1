fb9a8f2bd39af70f34053f3b3fa041850300391c
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-24 12:57:15 +0300

    checks if a character is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	for (c = 48; c <= 59; c++)
+	{
+		return (1);
+	}
+
+	return (0);
+}
