80a9e6335066cdaec781488cad32ed8fbec9f6ca
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-24 22:03:58 +0300

    random

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,8 +2,9 @@
 #include "holberton.h"
 
 /*
- * print_remaining_days() - takes a date and prints how many days are
+ * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
+ * Description: prints the date
  * @month: month in number format
  * @day: day of month
  * @year: year
