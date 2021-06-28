b15b15913426301500876f714e5c5c38066f3540
firdaushassansalim <firdaushassan033@gmail.com>
2021-06-28 15:55:58 +0300

    Swaps the values of 2 int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,14 @@
 
 /**
  * swap_int - swaps the values of a and b
- * @a: pointer int = 98
- * @b: pointer int = 42
+ * @a: pointer 1
+ * @b: pointer 2
  * Return; 0
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
