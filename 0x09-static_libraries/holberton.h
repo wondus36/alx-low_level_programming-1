077aaf56983186da8898b5da1e45f8b8940ffbc1
firdaushassansalim <firdaushassan033@gmail.com>
2021-07-09 21:00:25 +0300

    updates

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -3,7 +3,7 @@
 
 /*
  * File: holberton.h
- * Auth: Brennan D Baraban
+ * Auth: Firdaus
  * Desc: Header file containing the prototypes of all functions
  *       included in the static library libholberton.a.
  */
