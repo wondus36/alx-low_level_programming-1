3d5ed15df8be4710cf7bb200d643c799c29f7dab
firdaushassansalim <firdaushassan033@gmail.com>
2021-07-09 20:41:18 +0300

    done tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,21 @@
 #include "holberton.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area
- *		pointed to by @s with the constant byte @c
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
 
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
 
-	return (memory);
+	return (s);
 }
