92d2272a95e3d8778fea85390984c4567ecda86d
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-21 13:15:34 +0300

    fixed bug

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/kjnat/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/gm
 export LD_PRELOAD=/tmp/nrandom.so
