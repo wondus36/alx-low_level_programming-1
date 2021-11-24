d9bddae440388d39cf261e869d1ed0b2d16d97da
firdaushassansalim <firdaushassan033@gmail.com>
2021-11-24 20:32:44 +0300

    completed tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,16 +12,16 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
 	list_t *new;
+	char *dup_str;
+	int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
+	dup_str = strdup(str);
+	if (dup_str == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -30,7 +30,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (len = 0; str[len];)
 		len++;
 
-	new->str = dup;
+	new->str = dup_str;
 	new->len = len;
 	new->next = *head;
 
